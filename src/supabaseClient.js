a024198e0808c740baaf1073686a4ef0d7dabe2c
josearun85 <josearun85@gmail.com>
2025-04-12 17:13:28 +0530

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,20 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-)
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+const customFetch = (url, options) => {
+  options.headers = {
+    ...options.headers,
+    Accept: 'application/json'
+  }
+  return fetch(url, options)
+}
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  global: {
+    fetch: customFetch
+  }
+})
 
 export default supabase
