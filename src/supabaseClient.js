d213f840ef3fc45f2a6da3c9c49f067eb64c2d88
Arun Jose <arun@exactspace.co>
2025-04-12 21:02:43 +0530

    cm

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,9 +3,9 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-const customFetch = (url, options) => {
+const customFetch = (url, options = {}) => {
   options.headers = {
-    ...options.headers,
+    ...(options.headers || {}),
     Accept: 'application/json'
   }
   return fetch(url, options)
