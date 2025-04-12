85ef898f78cd70663afefcd8c06e527b9b871e9d
josearun85 <josearun85@gmail.com>
2025-04-12 17:19:05 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,11 @@
+import { Link } from 'react-router-dom'
 
+export default function Navbar() {
+  return (
+    <nav style={{ padding: '1rem', borderBottom: '1px solid #ddd' }}>
+      <Link to="/customers">Customers</Link> |{' '}
+      <Link to="/orders">Orders</Link> |{' '}
+      <Link to="/invoices">Invoices</Link>
+    </nav>
+  )
+}
