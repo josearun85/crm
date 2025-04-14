bfca76dc58af7ecb09379844bad1a0f2f4f19ed5
Arun Jose <arun@exactspace.co>
2025-04-14 21:59:27 +0530

    up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,9 +16,7 @@ export default function Navbar({ session }) {
       <div className="navbar-left">
         <img src="/logo.png" alt="Logo" className="navbar-logo" />
         <Link to="/customers" className={location.pathname.startsWith('/customers') ? 'active' : ''}>Customers</Link>
-        <Link to="/inventory" className={location.pathname.startsWith('/inventory') ? 'active' : ''}>Inventory</Link>
-        <Link to="/vendors" className={location.pathname.startsWith('/vendors') ? 'active' : ''}>Vendors</Link>
-        <Link to="/payments" className={location.pathname.startsWith('/payments') ? 'active' : ''}>Payments</Link>
+        <Link to="/orders" className={location.pathname.startsWith('/order') ? 'active' : ''}>Orders</Link>
       </div>
       <div className="navbar-right">
         <button onClick={handleLogout}>Logout</button>
