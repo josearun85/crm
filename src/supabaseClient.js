3e100b8545a418d3ebd042b3150995e79c4e2d0e
Arun Jose <arun@exactspace.co>
2025-04-14 09:48:58 +0530

    up

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -16,14 +16,6 @@ supabase.auth.onAuthStateChange((event, session) => {
     console.log('User signed out')
   }
 })
-supabase.auth.onTokenRefresh((event, session) => {
-  console.log('Token refreshed:', session)
-})
-supabase.auth.onSessionChange((event, session) => {
-  console.log('Session changed:', session)
-})
-supabase.auth.onUserUpdate((event, session) => {
-  console.log('User updated:', session.user)
-})
+
 
 export default supabase
